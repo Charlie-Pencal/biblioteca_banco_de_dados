bffe8d981aa5cdea1907ce91cb23d0eb34988b0b
Charlie-Pencal <cpencal@gmail.com>
2023-08-01 18:23:19 -0300

    população nas tabelas

---
+++ scripts/registros_biblioteca.sql
@@ -0,0 +1,75 @@
+INSERT INTO
+	ENDERECO
+VALUES
+	(1,'82020490','Rua Santa Bertila Boscardin',123,'bloco 2 ap 1','Santa Felicidade','Curitiba','Paraná','Brasil'),
+	(2,'87045630','Rua Poeta Mário de Andrade',321,'nenhum','Jardim Atlanta','Maringá','Paraná','Brasil'),
+	(3,'83321280','Rua Carlos Pereira Canani',234,'bloco 1 ap 7','Vargem Grande','Pinhais','Paraná','Brasil'),
+	(4,'81265350','Rua Angélica Pavelski',432,'nenhum','Augusta','Curitiba','Paraná','Brasil'),
+	(5,'81590170','Rua Walter Jorge de Paiva',345,'nenhum','Uberaba','Curitiba','Paraná','Brasil'),
+	(6,'83326090','Rua Antônio Kowalczuck',543,'nenhum','Atuba','Pinhais','Paraná','Brasil'),
+	(7,'83327206','Rua das Amoreiras',456,'nenhum','Alphaville Graciosa','Pinhais','Paraná','Brasil'),
+	(8,'81480354','Rua Irmã Faustina Kowalska',654,'nenhum','Tatuquara','Curitiba','Paraná','Brasil'),
+	(9,'82310220','Rua Joana Z. Kurek',567,'nenhum','São Braz','Curitiba','Paraná','Brasil'),
+	(10,'81900240','Rua Arapoti',765,'nenhum','Sitio Cercado','Curitiba','Paraná','Brasil');
+
+INSERT INTO 
+	ENDERECO 
+VALUES
+	(11,'83406460','Travessa Noruega',404,'Não tem','Monza','Colombo','Paraná','Brasil'),
+	(12,'84061252','Rua Angico Branco',203,'perto da sapataria','Contorno','Ponta Grossa','Paraná','Brasil'),
+	(13,'83327146','Rua dos Flamingos',555,'nenhum','Alphaville Graciosa','Pinhais','Paraná','Brasil'),
+	(14,'85507140','Rua Taubaté',333,'nada','São Roque','Pato Branco','Paraná','Brasil');
+
+
+INSERT INTO
+	EDITORAS
+VALUES
+	(1,'Editora Vera Cruz LTDA','LeYa','14120675000189',14);
+    
+INSERT INTO
+	EDITORAS
+VALUES
+	(2,'Editora Rocco LTDA','Rocco','27673817000187',13),
+	(3,'Annunziato Sousa LTDA','Annunziato','93435564000138',12),
+	(4,'Editora Macário Moura LTDA','Macário','27673817000187',11);
+    
+INSERT INTO
+	PESSOAS
+VALUES
+	(1,'Daniel Valladares Camara','27421635149','2002-03-10','brasileira','daniel.camara@geradornv.com.br',1),
+	(2,'Lilian Assis Kuster','48243902309','1964-03-22','brasileira','lilian.kuster@geradornv.com.br',2),
+	(3,'Moacir Reis de Oliveira','96753548793','1999-12-18','brasileira','moacir.oliveira@geradornv.com.br',3),
+	(4,'Higor Lourenço Trancoso','79655117545','2005-02-25','brasileira','higor.trancoso@geradornv.com.br',4),
+	(5,'Gleyce Paes Amaral','98329255255','1979-10-11','brasileira','gleyce.amaral@geradornv.com.br',5),
+	(6,'Dionísio Barros Pinho','36079305119','1940-09-26','brasileira','dionisio.pinho@geradornv.com.br',6),
+	(7,'Calebe Ferreira Vitorino','75897753717','2005-11-27','brasileira','calebe.vitorino@geradornv.com.br',7),
+	(8,'Estefani Salles Nazare','51932273336','2003-08-26','brasileira','estefani.nazare@geradornv.com.br',8),
+	(9,'Kauê Gonçalves Mayerhofer','22825180556','1951-04-04','brasileira','kaue.mayerhofer@geradornv.com.br',9),
+	(10,'Débora Marques Lopez','87105339233','1983-06-08','brasileira','debora.lopez@geradornv.com.br',10);
+    
+INSERT INTO
+	USUARIO
+VALUES
+	(1,722787,1),
+	(2,892397,2),
+	(3,983173,3),
+	(4,229622,4),
+	(5,436242,5);
+    
+INSERT INTO
+	AUTORES
+VALUES
+	(1,1,6),
+	(2,1,7),
+	(3,1,8),
+	(4,1,9),
+	(5,1,10);
+
+INSERT INTO
+	LIVROS
+VALUES
+	(1,'Brother Imaginario','Fantasia',258,1,1,2),
+	(2,'Por do sol em marte','Aventura',175,0,2,2),
+	(3,'Codigo deu Erro','Suspense',231,0,3,1),
+	(4,'Culpe o Bootstrap','Autoajuda',150,0,4,3),
+	(5,'Cardinalidade','Romance',230,1,5,4);
